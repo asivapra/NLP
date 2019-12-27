2641608bc4afbd31f590fb0e7b6be6164e619ec5
Arapaut Sivaprasad <avs@webgenie.com>
2019-12-27 17:28:50 +1100

    After deleting the unused functions and commenting the code

--- STLP/NLP/nlp_parallel.py
+++ STLP/NLP/nlp_parallel.py
@@ -8,7 +8,7 @@ import re
 import time
 from nltk.corpus import stopwords
 import multiprocessing as mp
-import warnings
+# import warnings
 # warnings.filterwarnings('ignore')
 html1 = ''
 html2 = ''
@@ -24,59 +24,20 @@ kw2 = ''
 desc2 = ''
 title1 = ''
 title2 = ''
+doc1_segments = []
+doc2_segments = []
+n_doc1_segments = 0
+n_doc2_segments = 0
 
 ct0 = time.perf_counter()
-print("14:Starting to read dictionary and stopwords")
+print("Starting to read dictionary and stopwords")
 nlp = spacy.load("en_core_web_md")
 sr = stopwords.words('english')
 et = time.perf_counter() - ct0
-print("18:Finished reading: {:0.2f} sec".format(et))
-
-
-def read_words():
-    """
-    This function is not used
-    :return:
-    """
-    global nlp, sr
-    print("Reading in words and stopwords")
-    # Load only once
-    try:
-        if nlp:
-            pass
-    except:
-        nlp = spacy.load("en_core_web_md")
-        pass
-
-    # Load only once
-    try:
-        if sr[0]:
-            pass
-    except:
-        sr = stopwords.words('english')
-        pass
-    print("Finished reading in words and stopwords")
+print("Finished reading: {:0.2f} sec".format(et))
 
 
 # ------Functions
-def tokenize(text):
-    t0 = []
-    # Split the text into words separated by any of these chars
-    p = re.compile('[-_\", {};:=?\[\]\(\)\'.]')
-
-    # Get the word list into an array, t
-    t = p.split(text)
-    lt = len(t)
-
-    # Add the 4+ chars words into an array, t1. Discard the smaller words
-    for i in range(lt):
-        w = t[i]
-        lw = len(w)
-        if lw > 3:
-            t0.append(w)
-    return t0
-
-
 def Lemmatise(text):
     global nlp,sr
     # Implementing lemmatization
@@ -85,9 +46,9 @@ def Lemmatise(text):
     lem = nlp(text)
     # finding lemma for each word
     for word in lem:
-        if (p.findall(str(word))):
+        if p.findall(str(word)):
             lemma = word.lemma_.lower()
-            if (len(lemma) > 3):
+            if len(lemma) > 3:
                 if str(lemma) not in (sr):
                     lt.append(lemma)
     return lt
@@ -97,18 +58,6 @@ def CountWords(t):
     global counts
     from collections import Counter
     counts = Counter(t)
-
-
-def get_and_write_urls(nurls):
-    # get all URLs and write to a local file. Once only
-    for i in range(nurls):
-        url = urls[i]
-        file = "Files/url" + str(i) + ".html"
-        print(file)
-        response = urllib.request.urlopen(url)
-        html = response.read()
-        with open(file, 'wb') as f:
-            f.write(html)
 
 
 def ReadUrls(i, j):
@@ -133,12 +82,10 @@ def ReadUrls(i, j):
     # Read the first URL or its stored file content
     try:
         with open(file1, "rb") as f:
-            filecontent = bytes(f.read())
-            text1, title1, kw1, desc1 = text_from_html(filecontent,file1)
-            print(file1, text1)
+            file_content = bytes(f.read())
+            text1, title1, kw1, desc1 = text_from_html(file_content)
             # Take the lemmas of words within the text in pages. The 'text1' comes from the <body> and omits title
             t1 = Lemmatise(text1)
-
             # Take the lemmas from the title of the page. This is NOT used at present.
             t1t = Lemmatise(str(title1))
     except Exception as e:
@@ -147,12 +94,11 @@ def ReadUrls(i, j):
     if err:
         # If stored file does not exist, then get the content from the URL and write it out in stored file.
         # Since the same URL is used in many comparisons, faster to store it and read from it for subsequent calls.
-        # print('Reading from URL:')
         response = urllib.request.urlopen(url1)
         html1 = response.read()
         with open(file1, 'wb') as f:
             f.write(html1)
-        text1, title1, kw1, desc1 = text_from_html(html1,url1)
+        text1, title1, kw1, desc1 = text_from_html(html1)
         t1 = Lemmatise(text1)
         t1t = Lemmatise(str(title1))
     err = 0
@@ -160,12 +106,10 @@ def ReadUrls(i, j):
     # Read the seconds URL or its stored file content
     try:
         with open(file2, "rb") as f:
-            filecontent = bytes(f.read())
-            text2, title2, kw2, desc2 = text_from_html(filecontent,file2)
-            print(file2, text2)
+            file_content = bytes(f.read())
+            text2, title2, kw2, desc2 = text_from_html(file_content)
             t2 = Lemmatise(text2)
             t2t = Lemmatise(str(title2))
-            # print('Reading from file 2:', title2)
     except Exception as e:
         print(e)
         err = 1
@@ -173,8 +117,8 @@ def ReadUrls(i, j):
         response = urllib.request.urlopen(url2)
         html2 = response.read()
         with open(file2, 'wb') as f:
-             f.write(html2)
-        text2, title2, kw2, desc2 = text_from_html(html2,url2)
+            f.write(html2)
+        text2, title2, kw2, desc2 = text_from_html(html2)
         t2 = Lemmatise(text2)
         t2t = Lemmatise(str(title2))
 
@@ -185,119 +129,73 @@ def ReadUrls(i, j):
     for key, val in counts.items():
         item = str(val) + ':' + str(key.lower())
         t1w.append(item)
-    t1w.sort(key=lambda fname: int(fname.split(':')[0]), reverse=True)
+    t1w.sort(key=lambda f_name: int(f_name.split(':')[0]), reverse=True)
     CountWords(t2)
     t2w = []
     for key, val in counts.items():
         item = str(val) + ':' + str(key.lower())
         t2w.append(item)
-    t2w.sort(key=lambda fname: int(fname.split(':')[0]), reverse=True)
+    t2w.sort(key=lambda f_name: int(f_name.split(':')[0]), reverse=True)
 
 
 def GetDocSegments():
-    global doc1_segs, doc2_segs, l, m
+    global doc1_segments, doc2_segments, n_doc1_segments, n_doc2_segments
     # Split and save the text as 100 word segments
-    doc1_segs = []
-    doc2_segs = []
+    doc1_segments = []
+    doc2_segments = []
     text = ''
-    # Split t1 and t2 into text of 100 words
-    l = len(t1)
-    n = int(l / 100)
+    segment_size = 100
+    # Split t1 and t2 into text of segment_size words
+    l1 = len(t1)
+    n = int(l1 / segment_size)
     for i in range(n):
-        b = i * 100
-        e = b + 100
+        b = i * segment_size
+        e = b + segment_size
         for j in range(b, e):
             text += t1[j] + ' '
-        doc1_segs.append(text)
+        doc1_segments.append(text)
         text = ''
 
-    l = len(t2)
-    n = int(l / 100)
+    l2 = len(t2)
+    n = int(l2 / segment_size)
     for i in range(n):
-        b = i * 100
-        e = b + 100
+        b = i * segment_size
+        e = b + segment_size
         for j in range(b, e):
             text += t2[j] + ' '
-        doc2_segs.append(text)
+        doc2_segments.append(text)
         text = ''
 
-    l = len(doc1_segs)
-    m = len(doc2_segs)
+    n_doc1_segments = len(doc1_segments)
+    n_doc2_segments = len(doc2_segments)
 
 
-def CalculateSimilarity(i, j, nurls):
-#    sim = 0.00
+def CalculateSimilarity():
+    """
+    The logic of this function is confusing, but it works!
+    Must try to recollect the logic. DO NOT CHANGE IT NOW.
+    :return: avg
+    """
     sims = []
-    tot = 0.00
-    k = nurls
-    if (l < k):
-        k = l
-    if (m < k):
-        k = m
-    if (k == 0):
+    k = n_urls
+    if n_doc1_segments < k:
+        k = n_doc1_segments
+    if n_doc2_segments < k:
+        k = n_doc2_segments
+    if k == 0:
         return 0
     for i in range(k):
         for j in range(k):
-            doc1 = nlp(doc1_segs[i])
-            doc2 = nlp(doc2_segs[j])
+            doc1 = nlp(doc1_segments[i])
+            doc2 = nlp(doc2_segments[j])
             sim = doc1.similarity(doc2)
             sims.append(sim)
     sims.sort(reverse=True)
+    tot = 0.00
     for i in range(k):
         tot += sims[i]
     avg = tot / k
     return avg
-
-
-def CalculateWordFrequency():
-    tw1f = []
-    tw2f = []
-    # ------------t1--------------
-#    lw = len(t1w)
-    tot = 0
-    # Count the total number of words
-    for i in range(0, 3):
-        fields = t1w[i].split(":")
-        tot += int(fields[0])
-    # Take the percentage of top 10 words in t1w
-    for i in range(0, 3):
-        fields = t1w[i].split(":")
-        c = int(fields[0])
-        w = fields[1]
-        f = str(round(float(c / tot * 100), 2))
-        fw = f + ":" + w
-        tw1f.append(fw)
-    # ------------t2--------------
-#    lw = len(t2w)
-    tot = 0
-    # Count the total number of words
-    for i in range(0, 3):
-        fields = t2w[i].split(":")
-        tot += int(fields[0])
-    for i in range(0, 3):
-        fields = t2w[i].split(":")
-        c = int(fields[0])
-        w = fields[1]
-        f = str(round(float(c / tot * 100), 2))
-#        fw = f + ":" + w
-        tw2f.append(w)
-    # Compare the two to get the word frequency
-    lw1 = len(tw1f)
-    lw2 = len(tw2f)
-    mf = 0.00
-    tf = 0.00
-    for i in range(lw2):
-        w2 = tw2f[i]
-        tf = 0.00
-        for j in range(lw1):
-            fields = tw1f[j].split(":")
-            f1 = float(fields[0])
-            w1 = fields[1]
-            tf += f1
-            if w1 == w2:
-                mf += f1
-    freq = round(mf / tf, 2)
-    return freq
 
 
 def tag_visible(element):
@@ -309,7 +207,7 @@ def tag_visible(element):
     return True
 
 
-def text_from_html(body,src):
+def text_from_html(body):
     """
     The URL page content is processed to take the text from visible parts only.
     BeautifulSoup parses the page content
@@ -340,115 +238,7 @@ def text_from_html(body,src):
     return u" ".join(t.strip() for t in visible_texts), title, keywords, description
 
 
-def SimilarityScore(t1, t2):
-    doc1 = nlp(t1)
-    doc2 = nlp(t2)
-    sim = doc1.similarity(doc2)
-    return sim
-
-
-def Domain_match(url1, url2):
-    ldv = 0.00
-    f1 = url1.split('/')
-    d1 = f1[2].replace('www.', '')
-    f2 = url2.split('/')
-    d2 = f2[2].replace('www.', '')
-    if d1 == d2:
-        ldv = 1.0
-    return ldv
-
-
-def OneComparison():
-    # Read the HTML contents and lemmatise into two arrays, 't1' and 't2'
-    i = 0
-    j = 2
-    ReadUrls(i, j)
-#    sk = 0.00
-    st = SimilarityScore(str(title1), str(title2))
-    sk = SimilarityScore(kw1, kw2)
-    sd = SimilarityScore(desc1, desc2)
-    print(title1, '|', kw1, '|', desc1, '|', st, '|', sk, '|', sd, '\n')
-    print(title2, '|', kw2, '|', desc2, '|', st, '|', sk, '|', sd, '\n')
-
-
-def one_pair(i, j):
-    print("i,j:",i,j)
-    ReadUrls(i, j)
-    st = SimilarityScore(str(title1), str(title2))
-    sk = SimilarityScore(kw1, kw2)
-    sd = SimilarityScore(desc1, desc2)
-    dv = Domain_match(urls[i], urls[j])
-    GetDocSegments()
-    sc = CalculateSimilarity(i, j, nurls)
-    s = round(sc, 2)
-    matrix[i][j] = s
-    print(i, j, s)
-
-
-def MultipleComparisons():
-    global o
-    h = "#,"
-    o = ""
-    for i in range(nurls):
-        h += str(i) + ','
-
-    def AddTabs(i):
-        global o
-        for j in range(i):
-            o += ','
-
-    for i in range(nurls):
-        o = str(i)
-        AddTabs(i + 1)
-        for j in range(i, nurls):
-            ReadUrls(i, j)
-            st = SimilarityScore(str(title1), str(title2))
-            sk = SimilarityScore(kw1, kw2)
-            sd = SimilarityScore(desc1, desc2)
-            #        tv = Title_Match(t1t, t2t) # If any word in titles match, tf=0.1
-            dv = Domain_match(urls[i], urls[j])
-            GetDocSegments()
-            sc = CalculateSimilarity(i, j, nurls)
-            wf = CalculateWordFrequency()
-            #        s = round((sc + wf + tv + dv),2)
-            #        s = round((sc + wf + dv + st + sk + sd),2)
-            s = round(sc, 2)
-            o += str(s) + ','
-        #    o += '\n'
-        print(o)
-
-
-def sequential():
-    """
-    This is to do the calculations sequentially. It is NOT being used anymore and its functionality may be broken.
-    :return:
-    """
-    for i in range(nurls):
-        for j in range(i + 1, nurls):
-            one_pair(i, j)
-    print(matrix)
-
-    # Print the matrix out for Excel as e.g...
-    # #,1,2,
-    # 0,0.97,0.95,
-    # 1,,0.96,
-    # 2,,,,
-
-    print("{},".format('#'), end='')
-    for i in range(1, nurls):
-        print("{},".format(i), end='')
-    print('')
-    for i in range(nurls):
-        print("{}".format(i), end='')
-        for j in range(nurls):
-            v = str(matrix[i][j])
-            if v == ' ':
-                v = ''
-            print("{},".format(v), end='')
-        print('')
-
-
-def par_ReadUrls(m,n,pairs,w,C_1D):
+def par_ReadUrls(m, n, pairs, w, C_1D):
     """
     :param m: start of the range of pairs to be processed
     :param n: end of the range
@@ -474,11 +264,12 @@ def par_ReadUrls(m,n,pairs,w,C_1D):
         j = int(i_j[1])
         ReadUrls(i, j)
         GetDocSegments()
-        sc = CalculateSimilarity(i, j, nurls)
+        sc = CalculateSimilarity()
         s = round(sc, 2)
         C_1D[k] = s
-    et1 = time.perf_counter() - ct1 - ct0
-    print("419:Worker{} Finished comparisons: {:0.2f} sec".format(w,et1))
+    ct2 = time.perf_counter()
+    et1 = ct2 - ct1
+    print("Worker {} Finished comparisons: {:0.2f} sec".format(w, et1))
 
 
 def par_compare():
@@ -497,11 +288,11 @@ def par_compare():
 
     """
     pairs = []
-    for i in range(nurls):
-        for j in range(i + 1, nurls):
+    for i in range(n_urls):
+        for j in range(i + 1, n_urls):
             k = str(i) + ',' + str(j)
             pairs.append(k)
-    print(len(pairs))
+    # print(len(pairs))
     C_1D = mp.RawArray('d', len(pairs))  # flat version of matrix C. 'd' = number
     num_workers = mp.cpu_count()
     chunk_size = len(pairs) // num_workers + 1
@@ -535,48 +326,51 @@ def par_compare():
         w.join()
     k = 0
 
-    nn = nurls
-    # Initialise a list of nurls*nurls with 0s to hold the cosine similarity scores.
-    matrix = [[0] * nurls for i in range(nn)]
+    # nn = n_urls
+    # Initialise a list of n_urls*n_urls with 0s to hold the cosine similarity scores.
+    matrix = [[0] * n_urls for i in range(n_urls)]
 
     # Print the top row values as we populate the matrix
     print("#,", end='')
-    for i in range(nn):
-        print("{},".format(i),end='')
+    for i in range(n_urls):
+        print("{},".format(i), end='')
 
         # C_1D is a 1-dimensional array that holds the values. It gets the values from all workers together
         # Split it into rows and columns in matrix
-        for j in range(i+1,nn):
+        for j in range(i+1, n_urls):
             matrix[i][j] = C_1D[k]
             k += 1
     print('')
-    for i in range(nn):
+    for i in range(n_urls):
         print("{},".format(i),end='')
-        for j in range(nn):
+        for j in range(n_urls):
             print("{},".format(matrix[i][j]), end='')
         print('')
 
     # Write the results into a file. This will be used to compare against a reference matrix for expected matches
     with open('Files/matrix.txt', 'w') as f:
-        for i in range(nurls):
-            for j in range(nurls):
+        for i in range(n_urls):
+            for j in range(n_urls):
                 f.write("{},".format(str(matrix[i][j])))
             f.write("\n")
 
     # Read in the ref_table for expected matches
     ref_table = [[0] * 30 for i in range(30)]
     j = 0
-    with open('ref_table.txt','r') as f:
+    with open('ref_table.txt', 'r') as f:
         lines = f.readlines()
         for i in range(len(lines)):
             lines[i].rstrip('\n\r')
             la = lines[i].split('\t')
-            print(len(la))
             ref_table[j] = la
             j += 1
-    print(ref_table)
+    # print(ref_table)
+
+
 # - Functions - End -----------------------------------------------------------------
 # Globals - Begin
+
+
 urls = [
     "https://www.ands.org.au/working-with-data/metadata/geospatial-data-and-metadata",
     "https://www.ands.org.au/guides/geospatial",
@@ -613,17 +407,12 @@ urls = [
     "https://en.wikipedia.org/wiki/Titanic_(1997_film)",
     "https://en.wikipedia.org/wiki/Titanic_(1953_film)",
 ]
-nurls = len(urls)
-nurls = 3
+n_urls = len(urls)
+n_urls = 3
 # Globals - End
 #
 # -----------------------------------------------------------------------------------------------------------------
 # Call the functions
-#read_words()
-# OneComparison()
-# MultipleComparisons()
-#sequential()
-# get_and_write_urls(nurls)
 if __name__ == '__main__':
     """
     The function, 'par_compare()', does the following:
@@ -632,5 +421,5 @@ if __name__ == '__main__':
     2. Split the pairs array into chunks based on the number of CPUs used.
     """
     par_compare()
-    etime = time.perf_counter() - ct0
-    print("Line 574:Total Time: {:0.2f} sec".format(etime))
+    et1 = time.perf_counter() - ct0
+    print("Total Time: {:0.2f} sec".format(et1))
